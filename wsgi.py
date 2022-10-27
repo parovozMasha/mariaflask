9b953f6e4c393ae3f80abadda143a1cf8429f85d
masha <mdegtareva928@gmail.com>
2022-10-27 12:50:12 +0300

    init

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,13 @@
 # grup = {1:'ПРи201', 2: 'ПИ-201', 3: 'ИСТ-201' , 4: 'ИВТ-201' }
 
-GROUP = 1
+GROUP = 2
 if  GROUP == 1:
     from apppri import app
 elif GROUP == 2:
     from apppi import app
 elif GROUP == 3:
     from appist import app
-elif GROUP == 4:
+elif GROUP == 4 :
     from appivt import app
 
 
